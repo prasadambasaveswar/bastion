2fe9dd527ec40b4c3c705dd58fb4aaf8964f0371
prasadambasaveswar <basaveswar01@gmail.com>
2019-04-16 20:02:07 +0530

    Create dev.sh

---
+++ dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DBPASSWORD="of15*XIpy&IAh9dWOngZWg8K#9p4qFUf"
+export DBUSER="otrapp"
+export DBNAME="llldevRstOtr"
+export DBHOST="dev-otr-rds01.c61s5rfb002a.us-west-2.rds.amazonaws.com"
+export BASTIONHOST="dev-otr-bastion01"
